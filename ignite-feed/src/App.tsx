cf3395ec85709fe9206d26ea791038c6a71c0db4
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-05-27 12:24:30 -0300

    feat(ignite-feed): add sidebar component

--- ignite-feed/src/App.tsx
+++ ignite-feed/src/App.tsx
@@ -1,6 +1,19 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+
+import "./app.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
-  return <Header/>
-}
+  return (
+    <div>
+      <Header />
 
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Main</main>
+      </div>
+    </div>
+  );
+}
---
+++ ignite-feed/src/components/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import { PencilLine  } from "@phosphor-icons/react";
+import styles from "./styles.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1629904853716-f0bc54eea481?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/ms-lucas.png" />
+
+        <strong>Lucas Moreira</strong>
+        <span>Desenvolvedor Web</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
