74522be61307a6a2aa00119f4fe6914efa226f5f
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-06-07 11:54:24 -0300

    feat(pomodoro-timer): add history page

--- pomodoro-timer/src/pages/History/index.tsx
+++ pomodoro-timer/src/pages/History/index.tsx
@@ -1,3 +1,30 @@
+import { HistoryList, HistoryWrapper, Status } from "./styles";
+
 export function History() {
-    return <h1>History</h1>
+    return (
+        <HistoryWrapper>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há cerca de 2 meses</td>
+                            <td><Status statusColor="green">Concluído</Status></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryWrapper>
+    )
 }
